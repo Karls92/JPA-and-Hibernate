455b55bbff39726eb9db9bcdd2c723628b3fa718
Karls92 <nbravoalcala@gmail.com>
2022-10-13 19:45:45 -0400

    the Customer class was completed with all the configurations.

--- CustomerManager/src/main/java/com/softwaredevone/CustomerManager/Customer.java
+++ CustomerManager/src/main/java/com/softwaredevone/CustomerManager/Customer.java
@@ -1,5 +1,61 @@
 package com.softwaredevone.CustomerManager;
 
-public class Customer {
+import java.io.Serializable;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "customer")
+public class Customer implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name = "id", unique = true)
+	private int id;
+	
+	@Column(name = "firstname", nullable = false)
+	private String firstName;
+	
+	@Column(name = "lastname", nullable = false)
+	private String lastName;
+	
+	@Column(name = "age", nullable = false)
+	private int age;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
 }
