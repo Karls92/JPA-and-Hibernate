6e30228b27cb93721a3caa59bcb5bc6786a803e8
Karls92 <nbravoalcala@gmail.com>
2022-10-13 19:07:51 -0400

    Start the jpa with hibernate vol.1 project

---
+++ CustomerManager/src/main/java/com/softwaredevone/CustomerManager/App.java
@@ -0,0 +1,13 @@
+package com.softwaredevone.CustomerManager;
+
+/**
+ * @author CARMEN BRAVO
+ * @version 1.0
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to JPA with Hibernate!" );
+    }
+}
